7428744c7e3d09b4ba3cbe764a91d32e721972d9
Yoni Widhi Cahyadi <yoni@mit.id>
2022-06-29 09:45:10 +0700

    Update: User Seeder Update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\{User , Student};
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
@@ -18,5 +18,22 @@ class UserSeeder extends Seeder
             ['role' => 'admin', 'username' => 'yoniwidhi', 'name' => 'Yoni Widhi C', 'email' => 'yoniwidhi@email.com', 'password' => \Hash::make('thispassword'), 'is_active' => '1', 'created_at' => date('Y-m-d H:i:s', time())],
             ['role' => 'admin', 'username' => 'admin2', 'name' => 'Admin 1', 'email' => 'admin@email.com', 'password' => \Hash::make('thispassword'), 'is_active' => '1', 'created_at' => date('Y-m-d H:i:s', time())],
         ]);
+
+        $students = Student::get();
+        $insertDb = [];
+
+        foreach ($students as $student) {
+            $insertDb[] = [
+                'role' => 'student',
+                'username' => explode(' ', $student->name)[0],
+                'name' => $student->name,
+                'email' => explode(' ', $student->name)[0] . '@email.com',
+                'password' => \Hash::make('thispassword'),
+                'is_active' => '0',
+                'created_at' => date('Y-m-d H:i:s', time())
+            ];
+        }
+
+        User::insert($insertDb);
     }
 }
